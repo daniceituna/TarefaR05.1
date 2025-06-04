49913fb35e1e16a1b1c73694071bfe4a8e8304a2
daniceituna <danicancela22@gmail.com>
2025-06-04 20:31:24 +0200

    80%

--- src/Repository/PedidoRepository.php
+++ src/Repository/PedidoRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Pedido;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -11,33 +12,17 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class PedidoRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private EntityManagerInterface $em)
     {
         parent::__construct($registry, Pedido::class);
     }
 
-    //    /**
-    //     * @return Pedido[] Returns an array of Pedido objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
-    //    public function findOneBySomeField($value): ?Pedido
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function save(Pedido $pedido, bool $flush = true): void
+    {
+        $this->em->persist($pedido);
+        if ($flush) {
+            $this->em->flush();
+        }
+    }
 }
